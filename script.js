6df87bf8f51761e88875a4ca819ac5a4b0c739db
Mark Trevor Ssali <trevomark12@gmail.com>
2025-08-24 14:57:49 +0300

    wamu-article-update1

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('nav-links')[0]
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    toggleButton.classList.toggle('active');
+    navbarLinks.classList.toggle('active');
 })
+
+
+// for animation
+
+const hiddenElements = document.querySelectorAll('.hidden');
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+hiddenElements.forEach(el => observer.observe(el));
